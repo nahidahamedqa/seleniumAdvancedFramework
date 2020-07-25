d88d8894c51d1072e8519a6b0043e4498acfc1e5
Nahid <nahid.ahamed.qa@gmail.com>
2020-07-25 11:14:57 -0400

    Create a new css locator for signInButton.

--- src/main/java/controller/LogInPageController.java
+++ src/main/java/controller/LogInPageController.java
@@ -6,8 +6,8 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LogInPageController {
-    @FindBy(xpath = "//*[@id='header']/div[2]/div/div/nav/div[1]/a") WebElement signInButton;
-
+    //@FindBy(xpath = "//*[@id='header']/div[2]/div/div/nav/div[1]/a") WebElement signInButton;
+@FindBy(css = "#header > div.nav > div > div > nav > div.header_user_info > a") WebElement signInButton;
     public LogInPageController(WebDriver driver){
         PageFactory.initElements(driver,this);
     }
